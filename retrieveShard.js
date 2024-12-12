b5acea7a10a77d6caf36d5643c5e3abf5d8db072
Meir Faraj <meir.faraj@mongodb.com>
2024-12-12 08:57:29 +0100

    save also in one .json
    save also in one .json

--- retrieveShard.js
+++ retrieveShard.js
@@ -1,9 +1,12 @@
 global=sh.getShardedDataDistribution().toArray();
+shard={}
+shard["globalDistribution"]=global
 fs.writeFileSync(
    'inputs/getShardedDataDistribution.json',
    EJSON.stringify(global, null, 2 )
 )
-
+shard["distribution_per_ns"]={}
+distribution_per_ns=shard["distribution_per_ns"]
 global.forEach(ns => {
     // Log the namespace being processed
     console.log(`Processing namespace: ${ns.ns}`);
@@ -13,7 +16,7 @@ global.forEach(ns => {
      if (dbName === "local"||dbName === "config"||dbName === "admin"||dbName === "test") return;
      // Run getShardDistribution on the current collection
      const distribution = db.getSiblingDB(dbName)[collectionName].getShardDistribution();
-
+     distribution_per_ns[ns.ns]=distribution
      fs.writeFileSync(
          `inputs/getShardedDataDistribution_${ns.ns}.json`,
           EJSON.stringify(distribution, null, 2 )
@@ -25,3 +28,9 @@ fs.writeFileSync(
    'inputs/serverStatus.json',
    EJSON.stringify(db.serverStatus(), null, 2 )
 )
+
+fs.writeFileSync(
+   'inputs/Sharding_distribution.json',
+   EJSON.stringify(shard, null, 2 )
+)
+
